ed72f74e1e7d3816132361e32e4918761cd9a420
Vikash Gupta <cse.vikashgupta@gmail.com>
2019-09-08 15:25:37 +0530

    * Getting Headers and Payloads from Request body

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 const http = require("http");
 // For Parsing request path from URL
 const url = require("url");
+// For decoding Strings of payloads
+const StringDecoder = require("string_decoder").StringDecoder;
 const PORT = 4500;
 var server = http.createServer(function(req, res) {
   // Getting the request url
@@ -17,16 +19,29 @@ var server = http.createServer(function(req, res) {
   const trimmedPath = path.replace("/^/+|/+$/g", "");
   // Getting Request type
   const requestType = req.method.toLowerCase();
-  res.end("Hello World");
-  // logging the path on request received
-  console.log(
-    "User requested on",
-    trimmedPath,
-    " with request type",
-    requestType,
-    " with queryString Object ",
-    queryStringObject
-  );
+  // Getting Headers from request
+  const headers = req.headers;
+  // Parsing payloads if any
+  const decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function() {
+    buffer += decoder.end();
+    res.end("Hello World");
+    // logging the path on request received
+    // console.log(
+    //   "User requested on",
+    //   trimmedPath,
+    //   " with request type",
+    //   requestType,
+    //   " with queryString Object ",
+    //   queryStringObject
+    // );
+    console.log("Request Headers ", headers);
+    console.log("Request Payload :", buffer);
+  });
 });
 
 server.listen(PORT, function(params) {
