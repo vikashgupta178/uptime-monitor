b00c80f9b8e5de8fbf05c73e5aa7bc75b1f0aa10
Vikash Gupta <cse.vikashgupta@gmail.com>
2019-09-08 15:00:08 +0530

    * Added url as library in app * Getting the path, method, queryString as Object from requested url.

--- index.js
+++ index.js
@@ -3,9 +3,30 @@
  */
 // Server Dependency for listening requests
 const http = require("http");
+// For Parsing request path from URL
+const url = require("url");
 const PORT = 4500;
 var server = http.createServer(function(req, res) {
+  // Getting the request url
+  var parsedUrl = url.parse(req.url, true);
+  // Getting the queryString Object
+  var queryStringAsObject = parsedUrl.query;
+  // Getting the request path
+  var path = parsedUrl.pathname;
+  // Trimming the unnecessary / or spaces
+  const trimmedPath = path.replace("/^/+|/+$/g", "");
+  // Getting Request type
+  const requestType = req.method.toLowerCase();
   res.end("Hello World");
+  // logging the path on request received
+  console.log(
+    "User requested on",
+    trimmedPath,
+    " with request type",
+    requestType,
+    " with queryString Object ",
+    queryStringObject
+  );
 });
 
 server.listen(PORT, function(params) {
